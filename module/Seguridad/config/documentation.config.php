e4dde35c4b8880a3c063646cb9a8f3a05510a91c
Alvaro Sanchez <asanchez@atijaguar.com>
2019-02-07 03:16:18 -0500

    Cración del proyecto

---
+++ module/Seguridad/config/documentation.config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'Seguridad\\V1\\Rest\\Trusua\\Controller' => [
+        'description' => 'Servicio para manejo de usuarios',
+        'collection' => [
+            'description' => 'Usuarios',
+        ],
+    ],
+    'Seguridad\\V1\\Rest\\Trperf\\Controller' => [
+        'description' => 'Servicio para admnistrar los perfiles',
+    ],
+];
